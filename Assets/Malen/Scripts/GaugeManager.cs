8e3e14217aba73dfcc5393aed0a65a8f43d0fe52
Malen <raspbell.caf2@gmail.com>
2024-03-23 15:13:17 +0900

    HPバー関連5

--- Assets/Malen/Scripts/GaugeManager.cs
+++ Assets/Malen/Scripts/GaugeManager.cs
@@ -7,16 +7,6 @@ using UnityEngine.UI;
 public class GaugeManager : MonoBehaviour
 {
 
-    [CreateAssetMenu(fileName = "HPBarPropety", menuName = "ScriptableObjects/HPBarPropety")]
-    public class HPBarPropety : ScriptableObject
-    {
-        [Header("HPカラー(多)")] public Color normalColor;
-        [Header("HPカラー(中)")] public Color damagedColor;
-        [Header("HPカラー(少)")] public Color dangerColor;
-        [Header("HPゲージ変動時間")] public float gaugeTransitionTime;
-        [Header("HPカラー基準値")] public float[] HPColorCriteria;
-    }
-
     [SerializeField] Image hpBarImage;
     [SerializeField] HPBarPropety hpBarPropety;
 
