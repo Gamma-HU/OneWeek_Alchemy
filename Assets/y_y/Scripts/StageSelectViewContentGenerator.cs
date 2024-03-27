731f60a21df6737da0027dc049b45847be64bca2
honebone <ryou4402@icloud.com>
2024-03-27 20:49:44 +0900

    クリア済みダンジョンを、未クリアダンジョンの後に表示するように

--- Assets/y_y/Scripts/StageSelectViewContentGenerator.cs
+++ Assets/y_y/Scripts/StageSelectViewContentGenerator.cs
@@ -13,6 +13,8 @@ public class StageSelectViewContentGenerator : MonoBehaviour
     private List<DungeonData> clearedDungeon = new List<DungeonData>();
     private List<DungeonData> unlockedDungeon = new List<DungeonData>();//解放されている未クリアのダンジョン
 
+    List<DungeonData> dungeonList = new List<DungeonData>();
+
     private Sprite background;
     private string dungeonName;
     private int difficulty;
@@ -24,16 +26,25 @@ public class StageSelectViewContentGenerator : MonoBehaviour
         gameManager = FindObjectOfType<GameManager>();
         clearedDungeon = gameManager.GetCleardDungeon();
         unlockedDungeon = gameManager.GetUnlockedDungeon();
+
+
         for (int i = 0; i < unlockedDungeon.Count; i++)
         {
             if (!clearedDungeon.Contains(unlockedDungeon[i]))
             {
-                GenerateContent(unlockedDungeon[i], i);
+                dungeonList.Add(unlockedDungeon[i]);
+                //GenerateContent(unlockedDungeon[i], i);
             }
+        }
+        if (clearedDungeon.Count > 0) { dungeonList.AddRange(clearedDungeon); }
+
+        for(int i = 0; i < dungeonList.Count; i++)
+        {
+            GenerateContent(dungeonList[i], i);
         }
         //for (int i = unlockedDungeon.Count; i < clearedDungeon.Count + unlockedDungeon.Count; i++)
         //{
-        //    GenerateContent(clearedDungeon[i], i);
+        //    GenerateContent(clearedDungeon[i], i + unlockedDungeon.Count);
         //}
     }
 
@@ -49,13 +60,13 @@ public class StageSelectViewContentGenerator : MonoBehaviour
 
         content.GetComponent<SetContentElement>().dungeonNum = i;
         //print((background, dungeonName, difficulty, dungeonInfo));
-        content.GetComponent<SetContentElement>().setElement(background, dungeonName, difficulty, dungeonInfo,gameManager.GetCleardDungeon().Contains(unlockedDungeon[i]));
+        content.GetComponent<SetContentElement>().setElement(background, dungeonName, difficulty, dungeonInfo,gameManager.GetCleardDungeon().Contains(dungeonList[i]));
     }
 
     public void SelectDungeon(int i)
     {
         alchemySceneManager.GetComponent<AlchemySceneManager>().ToggleSlots();
-        gameManager.SelectDungeon(unlockedDungeon[i]);
+        gameManager.SelectDungeon(dungeonList[i]);
 
     }
 }
